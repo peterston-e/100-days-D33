80c8322c08e748022fe5079e143f2c3edbaa0d8e
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2023-12-16 16:40:13 +0000

    initial commit for iss overhead app

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+MY_LAT = 50.860260
+MY_LNG = -0.130310
+#
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# print(data)
+#
+# lat = data['iss_position']['latitude']
+# long = data['iss_position']['longitude']
+# iss_position = (lat, long)
+# print(iss_position)
+# print(f"{lat}, {long}")
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-print(data)
 
-lat = data['iss_position']['latitude']
-long = data['iss_position']['longitude']
-iss_position = (lat, long)
-print(iss_position)
-print(f"{lat}, {long}")
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+time_now = datetime.now()
+print(time_now.hour)
